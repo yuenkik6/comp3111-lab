19072a61badd1afdc14c256f273274f22dcb0c17
Alex Yuen <yuenkik6@gmail.com>
2017-10-17 17:55:48 +0800

    Added Swagger

---
+++ Swagger2.java
@@ -0,0 +1,35 @@
+package com.example.bot.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.didispace.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Swagger")
+                .description("haha")
+                .termsOfServiceUrl("http://www.google.com/")
+                .contact("Alex")
+                .version("1.0")
+                .build();
+    }
+}
